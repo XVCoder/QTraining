1b8355a74ba91d36a73e7a7aa5aeb35c8d6f5e15
XnU132 <xiongyu132@gmail.com>
2020-06-22 11:02:21 +0800

    UI优化

--- QTraining/App.xaml.cs
+++ QTraining/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QTraining.Common;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,19 @@ namespace QTraining
     /// </summary>
     public partial class App : Application
     {
+        private static System.Threading.Mutex mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            mutex = new System.Threading.Mutex(true, "QTraining");
+            if (mutex.WaitOne(0, false))
+            {
+                base.OnStartup(e);
+            }
+            else
+            {
+                MessageHelper.Info(ResourceHelper.GetStrings("Message_ProgramIsRunning"), MessageBoxButton.OK, Panuon.UI.Silver.DefaultButton.YesOK, App.Current.MainWindow);
+                this.Shutdown();
+            }
+        }
     }
 }
